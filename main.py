f13e529037e7cd4a9253581b4a82cacc481632f3
LT_leonid <klayray10568@gmail.com>
2024-02-05 20:12:21 +0700

    modified:   main.py 	modified:   webcheck.py

--- main.py
+++ main.py
@@ -2,23 +2,33 @@ import telebot
 from urllib.parse import urlparse
 import requests
 from bs4 import BeautifulSoup
+from yandex_music import Client  # Добавлено для использования функции поиска музыки
 
 # Замените YOUR_BOT_TOKEN на токен вашего бота
 TOKEN = "6770656878:AAHL9nYL6TZZLfZoh-NFQgU01YH4dHp7uc0"
 
 bot = telebot.TeleBot(TOKEN)
+client = Client().init()  # Инициализация Yandex.Music Client
+
+type_to_name = {
+    'track': 'трек',
+    'artist': 'исполнитель',
+    'album': 'альбом',
+    'playlist': 'плейлист',
+    'video': 'видео',
+    'user': 'пользователь',
+    'podcast': 'подкаст',
+    'podcast_episode': 'эпизод подкаста',
+}
 
 def is_yandex_music_url(url):
     parsed_url = urlparse(url)
-    # Проверяем, содержит ли домен 'music.yandex'
     return 'music.yandex' in parsed_url.netloc
 
 def get_music_info(url):
     try:
         response = requests.get(url)
         soup = BeautifulSoup(response.text, 'html.parser')
-        
-        # Извлекаем информацию о музыке, например, заголовок трека
         track_title = soup.find('meta', property='og:title')
         
         if track_title:
@@ -29,18 +39,47 @@ def get_music_info(url):
         print(f"Error: {e}")
         return 'Произошла ошибка при получении информации о музыке'
 
+def send_search_request(query):
+    search_result = client.search(query)
+    return search_result
+
 @bot.message_handler(commands=['start'])
 def handle_start(message):
     bot.send_message(message.chat.id, 'Привет! Я бот для проверки ссылок на наличие музыки от Яндекс.Музыки.')
 
 @bot.message_handler(func=lambda message: True)
 def handle_message(message):
-    url = message.text
-    if is_yandex_music_url(url):
-        music_info = get_music_info(url)
-        bot.send_message(message.chat.id, f'Найдена музыка: {music_info}')
-    else:
-        bot.send_message(message.chat.id, 'Это не ссылка на Яндекс.Музыку.')
+    try:
+        url = message.text
+        if is_yandex_music_url(url):
+            music_info = get_music_info(url)
+            bot.send_message(message.chat.id, f'Найдена музыка: {music_info}')
+        else:
+            search_result = send_search_request(url)
+            if search_result.best:
+                type_ = search_result.best.type
+                best = search_result.best.result
+                if type_ in ['track', 'podcast_episode']:
+                    artists = ''
+                    if best.artists:
+                        artists = ' - ' + ', '.join(artist.name for artist in best.artists)
+                        best_result_text = best.title + artists
+                elif type_ == 'artist':
+                    best_result_text = best.name
+                elif type_ in ['album', 'podcast']:
+                    best_result_text = best.title
+                elif type_ == 'playlist':
+                    best_result_text = best.title
+                elif type_ == 'video':
+                    best_result_text = f'{best.title} {best.text}'
+                #
+                bot.send_message(message.chat.id, f'Лучший результат: {best_result_text}')
+            else:
+                bot.send_message(message.chat.id, 'Это не ссылка на Яндекс.Музыку.')
+    except:
+        bot.send_message(message.chat.id, f'Появилась ошибка. Но я должен дальше работать')
+        
 
 if __name__ == "__main__":
     bot.polling(none_stop=True)
+
--- webcheck.py
+++ webcheck.py
@@ -2,7 +2,7 @@
 
 client = Client('y0_AgAAAAAz5EsoAAG8XgAAAADwqKYWZE3PswvKQgqZ2J9pyYkaDrU94iw').init()
 client.users_likes_tracks()[0].fetch_track().download('example.mp3')'''
-import re
+'''import re
 
 def извлечь_номера_из_ссылки(ссылка):
     # Используем регулярные выражения для поиска числовых значений
@@ -23,3 +23,50 @@ if номера:
     print("Второй номер:", номера[1])
 else:
     print("Числовые значения не найдены в ссылке.")
+'''
+def send_search_request_and_print_result(query):
+    search_result = client.search(query)
+
+    text = [f'Результаты по запросу "{query}":', '']
+
+    best_result_text = ''
+    if search_result.best:
+        type_ = search_result.best.type
+        best = search_result.best.result
+
+        text.append(f'❗️Лучший результат: {type_to_name.get(type_)}')
+
+        if type_ in ['track', 'podcast_episode']:
+            artists = ''
+            if best.artists:
+                artists = ' - ' + ', '.join(artist.name for artist in best.artists)
+            best_result_text = best.title + artists
+            
+            # Добавляем информацию об авторе песни
+            if best.artists:
+                authors = ', '.join(artist.name for artist in best.artists)
+                text.append(f'Автор(ы): {authors}')
+        elif type_ == 'artist':
+            best_result_text = best.name
+        elif type_ in ['album', 'podcast']:
+            best_result_text = best.title
+        elif type_ == 'playlist':
+            best_result_text = best.title
+        elif type_ == 'video':
+            best_result_text = f'{best.title} {best.text}'
+
+        text.append(f'Содержимое лучшего результата: {best_result_text}\n')
+
+    if search_result.artists:
+        text.append(f'Исполнителей: {search_result.artists.total}')
+    if search_result.albums:
+        text.append(f'Альбомов: {search_result.albums.total}')
+    if search_result.tracks:
+        text.append(f'Треков: {search_result.tracks.total}')
+    if search_result.playlists:
+        text.append(f'Плейлистов: {search_result.playlists.total}')
+    if search_result.videos:
+        text.append(f'Видео: {search_result.videos.total}')
+
+    text.append('')
+    print('\n'.join(text))
